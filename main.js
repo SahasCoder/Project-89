cf82d2d65c80d8667daa5c2cf3232277ee9269f1
Code <90134769+SahasCoder@users.noreply.github.com>
2021-12-12 16:37:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function change1S(){
+        document.getElementById("player1Text").style.backgroundColor="grey";
+        document.getElementById("player1Text").style.color="white";
+}
+
+function change2S(){
+        document.getElementById("player2Text").style.backgroundColor="grey";
+        document.getElementById("player2Text").style.color="white";
+}
+
+function change1EX(){
+    document.getElementById("player1Text").style.backgroundColor="white";
+}
+
+function change2EX(){
+    document.getElementById("player2Text").style.backgroundColor="white";
+}
